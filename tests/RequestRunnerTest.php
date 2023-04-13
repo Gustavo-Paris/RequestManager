f5bdf1214412f790d870019c7d1cf36b6b078ccf
wbsartori <wesley.sartori@ixcsoft.com.br>
2023-04-13 17:55:27 -0300

    tests(request-manager): RequestManager
    units tests

--- tests/RequestRunnerTest.php
+++ tests/RequestRunnerTest.php
@@ -3,35 +3,59 @@
 namespace RequestManager\Tests;
 
 use PHPUnit\Framework\TestCase;
+use RequestManager\RequestRunner;
 use RequestManager\Requests\GuzzleRequest;
 
 class RequestRunnerTest extends TestCase
 {
     /**
-     * @var GuzzleRequest
+     * @var RequestRunner
      */
-    private $guzzleRequestMock;
+    private $requestRunner;
+
 
     /**
      * @return void
      */
-    public function setUp(): void
+    public function testSetClient()
     {
-        $this->guzzleRequest = new GuzzleRequest();
+        $this->requestRunner = new RequestRunner();
+        $actual = $this->requestRunner->setClient((new GuzzleRequest()));
+        $this->assertInstanceOf(RequestRunner::class, $actual);
     }
 
-    /**
-     * @return void
-     */
+    public function testSetHeaderAuth()
+    {
+        $this->requestRunner = new RequestRunner();
+        $actual = $this->requestRunner->setHeader(['header' => 'teste']);
+        $this->assertInstanceOf(RequestRunner::class, $actual);
+    }
+
     public function testBasicAuth()
     {
-        $this->guzzleRequestMock = $this->createPartialMock(GuzzleRequest::class, ['setHeader']);
-        $this->guzzleRequestMock->method('setHeader')->willReturnCallback(function () {
-            return ['header' => 'teste'];
-        });
+        $this->requestRunner = new RequestRunner();
+        $actual = $this->requestRunner->basicAuth('test', 'test');
+        $this->assertInstanceOf(RequestRunner::class, $actual);
+    }
 
-        $actual = $this->guzzleRequestMock->setHeader(['header' => 'teste']);
+    public function testBearerToken()
+    {
+        $this->requestRunner = new RequestRunner();
+        $actual = $this->requestRunner->bearerToken('02SAdasd0adsad');
+        $this->assertInstanceOf(RequestRunner::class, $actual);
+    }
 
-        $this->assertEquals(['header' => 'teste'], $actual);
+    public function testSetUri()
+    {
+        $this->requestRunner = new RequestRunner();
+        $actual = $this->requestRunner->setUri('https://api.com.br');
+        $this->assertInstanceOf(RequestRunner::class, $actual);
+    }
+
+    public function testSetData()
+    {
+        $this->requestRunner = new RequestRunner();
+        $actual = $this->requestRunner->setData(['data' => []]);
+        $this->assertInstanceOf(RequestRunner::class, $actual);
     }
 }
