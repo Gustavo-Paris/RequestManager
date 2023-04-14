87181a154dce6495e9922cc5034f78effa004854
wbsartori <wesley.sartori@ixcsoft.com.br>
2023-04-14 14:30:35 -0300

    tests(request-manager): RequestManager

--- tests/RequestRunnerTest.php
+++ tests/RequestRunnerTest.php
@@ -77,4 +77,124 @@ class RequestRunnerTest extends TestCase
         $actual = $this->requestRunner->run('post');
         self::assertEquals([], $actual);
     }
+
+    public function testRunIsNull()
+    {
+        $requestRunnerMock = $this->createMock(RequestRunner::class);
+        $requestRunnerMock->method('setClient')->willReturnCallback(function () {
+            return new GuzzleRequest();
+        });
+
+        $guzzleRequest = $this->createMock(GuzzleRequest::class);
+        $guzzleRequest->method('request')->willReturnCallback(function (){
+           return null;
+        });
+
+        $requestRunnerMock->setUri('https://api.com.br/');
+        $requestRunnerMock->setHeader(['header']);
+        $requestRunnerMock->setData(['multipart' => ['data' => 'dados']]);
+        $actual = $requestRunnerMock->run('post');
+
+        self::assertEmpty($actual, '');
+    }
+
+    public function testPost()
+    {
+        $guzzleRequest = $this->createMock(GuzzleRequest::class);
+        $guzzleRequest->method('request')->willReturnCallback(function (){
+            return ['response' => [
+                'data' => 'data'
+            ]];
+        });
+
+        $this->requestRunner = new RequestRunner();
+        $this->requestRunner->setClient($guzzleRequest);
+        $this->requestRunner->basicAuth('user', 'pass');
+        $this->requestRunner->setUri('https://api.com.br/');
+        $this->requestRunner->setHeader(['header']);
+        $this->requestRunner->setData(['data' => 'data']);
+
+        $actual = $this->requestRunner->post('post');
+
+        $this->assertEquals(['response' => [
+            'data' => 'data'
+        ]], $actual);
+    }
+
+    public function testGet()
+    {
+        $guzzleRequest = $this->createMock(GuzzleRequest::class);
+        $guzzleRequest->method('request')->willReturnCallback(function (){
+            return ['response' => [
+                'code' => 200,
+                'message' => 'Listed successfuly',
+                'data' => [],
+            ]];
+        });
+
+        $this->requestRunner = new RequestRunner();
+        $this->requestRunner->setClient($guzzleRequest);
+        $this->requestRunner->basicAuth('user', 'pass');
+        $this->requestRunner->setUri('https://api.com.br/');
+        $this->requestRunner->setHeader(['header']);
+        $this->requestRunner->setData(['data' => 'data']);
+
+        $actual = $this->requestRunner->get('get');
+
+        $this->assertEquals(['response' => [
+            'code' => 200,
+            'message' => 'Listed successfuly',
+            'data' => [],
+        ]], $actual);
+    }
+
+    public function testPut()
+    {
+        $guzzleRequest = $this->createMock(GuzzleRequest::class);
+        $guzzleRequest->method('request')->willReturnCallback(function (){
+            return ['response' => [
+                'code' => 200,
+                'message' => 'Updated successfuly',
+            ]];
+        });
+
+        $this->requestRunner = new RequestRunner();
+        $this->requestRunner->setClient($guzzleRequest);
+        $this->requestRunner->basicAuth('user', 'pass');
+        $this->requestRunner->setUri('https://api.com.br/');
+        $this->requestRunner->setHeader(['header']);
+        $this->requestRunner->setData(['data' => 'data']);
+
+        $actual = $this->requestRunner->put('put');
+
+        $this->assertEquals(['response' => [
+            'code' => 200,
+            'message' => 'Updated successfuly',
+        ]], $actual);
+    }
+
+    public function testDelete()
+    {
+        $guzzleRequest = $this->createMock(GuzzleRequest::class);
+        $guzzleRequest->method('request')->willReturnCallback(function (){
+            return ['response' => [
+                'code' => 200,
+                'message' => 'Deleted successfuly',
+            ]];
+        });
+
+        $this->requestRunner = new RequestRunner();
+        $this->requestRunner->setClient($guzzleRequest);
+        $this->requestRunner->basicAuth('user', 'pass');
+        $this->requestRunner->setUri('https://api.com.br/');
+        $this->requestRunner->setHeader(['header']);
+        $this->requestRunner->setData(['data' => 'data']);
+
+        $actual = $this->requestRunner->delete('delete');
+
+        $this->assertEquals(['response' => [
+            'code' => 200,
+            'message' => 'Deleted successfuly',
+        ]], $actual);
+    }
 }
